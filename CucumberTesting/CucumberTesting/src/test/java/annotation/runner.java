2da5bfd250afab67fa31992d078f964e07fbc197
Ayush Bansal <xs372-ayuban@xs-host603-037.xs.neuralcompany.team>
2023-09-19 13:30:10 +0530

    Report File Added

--- CucumberTesting/CucumberTesting/src/test/java/annotation/runner.java
+++ CucumberTesting/CucumberTesting/src/test/java/annotation/runner.java
@@ -2,8 +2,15 @@ package annotation;
 
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
 
 @RunWith(Cucumber.class) 
- 
+@CucumberOptions( 
+	features = "src/test/java", // where feature file is located
+	glue= {"annotation"},       // where step file is located
+	plugin = {"pretty", "html:target/Destination/report.html"},  // to create report in target/Destination folder
+	monochrome = true
+) 
 public class runner { 
 }
